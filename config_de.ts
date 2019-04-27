fa01403c2a789702b98cc2084a84ffc296f8282e
Lars Heuer <heuer@semagia.com>
2019-04-27 22:52:14 +0200

    More friendly layout for 3.2" displays

--- config_de.ts
+++ config_de.ts
@@ -19,59 +19,54 @@
         <translation>Bitte wähle einen Eintrag aus dem Menü.</translation>
     </message>
     <message>
-        <location filename="config.py" line="233"/>
+        <location filename="config.py" line="234"/>
         <source>Reboot</source>
         <translation>Reboot</translation>
     </message>
     <message>
-        <location filename="config.py" line="230"/>
+        <location filename="config.py" line="231"/>
         <source>It&apos;s recommended to restart the device.</source>
         <translation>Es wird ein Neustart des Gerätes empfohlen.</translation>
     </message>
     <message>
-        <location filename="config.py" line="230"/>
+        <location filename="config.py" line="231"/>
         <source>Do you want to reboot now?</source>
         <translation>Möchtest Du jetzt den Reboot durchführen?</translation>
     </message>
     <message>
-        <location filename="config.py" line="283"/>
+        <location filename="config.py" line="284"/>
         <source>SSH server</source>
         <translation>SSH-Server</translation>
     </message>
     <message>
-        <location filename="config.py" line="284"/>
+        <location filename="config.py" line="285"/>
         <source>VNC server</source>
         <translation>VNC-Server</translation>
     </message>
     <message>
-        <location filename="config.py" line="286"/>
-        <source>Enable / disable servers</source>
-        <translation>Server ein-/ausschalten</translation>
-    </message>
-    <message>
-        <location filename="config.py" line="293"/>
-        <source>The state of the servers is persistent.</source>
-        <translation>Der Status der Server ist persistent.</translation>
-    </message>
-    <message>
         <location filename="config.py" line="297"/>
-        <source>It remains after reboot / shutdown.</source>
-        <translation>Er bleibt nach einem Reboot erhalten.</translation>
+        <source>The state of the servers is persistent: It remains after reboot / shutdown.</source>
+        <translation>Der Status der Server ist persistent: Er bleibt nach einem Reboot erhalten.</translation>
     </message>
     <message>
-        <location filename="config.py" line="383"/>
+        <location filename="config.py" line="382"/>
         <source>Apply</source>
         <translation>Anwenden</translation>
     </message>
     <message>
-        <location filename="config.py" line="386"/>
+        <location filename="config.py" line="385"/>
         <source>Hostname</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="config.py" line="282"/>
+        <location filename="config.py" line="287"/>
         <source>Services</source>
         <translation>Dienste</translation>
+    </message>
+    <message>
+        <location filename="config.py" line="289"/>
+        <source>Enable / Disable</source>
+        <translation>Ein-/ausschalten</translation>
     </message>
 </context>
 </TS>
